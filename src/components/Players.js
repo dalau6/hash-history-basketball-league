fda21c214850a1cd8e584d910f439117c071cd78
David <Dalau6@gmail.com>
2018-01-13 20:02:13 -0800

    Code Splitting

--- src/components/Players.js
+++ src/components/Players.js
@@ -4,13 +4,17 @@ import Sidebar from './Sidebar'
 import { getPlayers } from '../api'
 import { parse } from 'query-string'
 import slug from 'slug'
+import {
+  TransitionGroup,
+  CSSTransition,
+} from 'react-transition-group'
 
 export default class Players extends Component {
   state = {
     players: [],
     loading: true,
   }
-  componentDidMount () {
+  componentDidMount() {
     const { location } = this.props
 
     location.search
@@ -19,10 +23,10 @@ export default class Players extends Component {
   }
   fetchPlayers = (teamId) => {
     getPlayers(teamId)
-    .then((players) => this.setState(() => ({
-      loading: false,
-      players,
-    })))
+      .then((players) => this.setState(() => ({
+        loading: false,
+        players,
+      })))
   }
   render() {
     const { players, loading } = this.state
@@ -43,36 +47,40 @@ export default class Players extends Component {
 
         <Route path={`${match.url}/:playerId`} render={({ match }) => {
           if (loading === true) return null
-          
+
           const {
             name, position, teamId, number, avatar, apg, ppg, rpg, spg,
           } = players.find((player) => slug(player.name) === match.params.playerId)
 
           return (
-            <div className='panel'>
-              <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
-              <h1 className='medium-header'>{name}</h1>
-              <h3 className='header'>#{number}</h3>
-              <div className='row'>
-                <ul className='info-list' style={{marginRight: 80}}>
-                  <li>Team 
+            <TransitionGroup className='panel'>
+              <CSSTransition key={location.key} timeout={250} classNames='fade'>
+                <div className='panel'>
+                  <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
+                  <h1 className='medium-header'>{name}</h1>
+                  <h3 className='header'>#{number}</h3>
+                  <div className='row'>
+                    <ul className='info-list' style={{ marginRight: 80 }}>
+                      <li>Team
                     <div>
-                      <Link style={{color: '#68809a'}} to={`/${teamId}`}>
-                        {teamId[0].toUpperCase() + teamId.slice(1)}
-                      </Link>
-                    </div>
-                  </li>
-                  <li>Position<div>{position}</div></li>
-                  <li>PPG<div>{ppg}</div></li>
-                </ul>
-                <ul className='info-list'>
-                  <li>APG<div>{apg}</div></li>
-                  <li>SPG<div>{spg}</div></li>
-                  <li>RPG<div>{rpg}</div></li>
-                </ul>
-              </div>
-            </div>
-          ) 
+                          <Link style={{ color: '#68809a' }} to={`/${teamId}`}>
+                            {teamId[0].toUpperCase() + teamId.slice(1)}
+                          </Link>
+                        </div>
+                      </li>
+                      <li>Position<div>{position}</div></li>
+                      <li>PPG<div>{ppg}</div></li>
+                    </ul>
+                    <ul className='info-list'>
+                      <li>APG<div>{apg}</div></li>
+                      <li>SPG<div>{spg}</div></li>
+                      <li>RPG<div>{rpg}</div></li>
+                    </ul>
+                  </div>
+                </div>
+              </CSSTransition>
+            </TransitionGroup>
+          )
         }} />
       </div>
     )
